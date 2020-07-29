ffc240861bb1267a390947394c86f996143c5b1a
alvaro <pedro_alvaro@outlook.com.pe>
2020-07-29 10:39:26 -0500

    primer servidor creado en go

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("public"))
+	http.Handle("/", fs)
+	log.Println("servidor iniciado en : http://127.0.0.1:8080")
+	http.ListenAndServe(":8080", nil)
+}
