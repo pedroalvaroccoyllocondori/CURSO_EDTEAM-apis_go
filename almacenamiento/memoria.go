5a54e70b5389356af46ace630c551cfa84d0af27
alvaro <pedro_alvaro@outlook.com.pe>
2020-07-30 11:13:50 -0500

    estructura de el proyecto

---
+++ almacenamiento/memoria.go
@@ -0,0 +1,5 @@
+package almacenamiento
+
+type Memoria struct {
+	actualID int
+}
---
+++ modelo/persona.go
@@ -0,0 +1,16 @@
+package modelo
+
+type Comunidad struct {
+	Nombre string
+}
+
+//tipo de dato comunidades slice
+type Comunidades []Comunidad
+
+type Persona struct {
+	Nombre string
+	Edad   uint8
+}
+
+//slice de perona tipo de dato
+type Personas []Persona
