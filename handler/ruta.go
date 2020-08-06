f8c3ef6dc40dc9446dd459a04b8251575c28cf8e
alvaro <pedro_alvaro@outlook.com.pe>
2020-08-05 21:21:05 -0500

    logica de autentificacion de token (con middleware)

--- handler/ruta.go
+++ handler/ruta.go
@@ -9,7 +9,7 @@ import (
 func RutaPersona(mux *http.ServeMux, almacenamiento Almacenamiento) {
 	h := nuevaPersona(almacenamiento)
 
-	mux.HandleFunc("/v1/personas/crear", middleware.Log(h.crear))
+	mux.HandleFunc("/v1/personas/crear", middleware.Log(middleware.Autentificacion(h.crear)))
 	mux.HandleFunc("/v1/personas/actualizar", h.actualizar)
 	mux.HandleFunc("/v1/personas/quitar", middleware.Log(h.quitar))
 	mux.HandleFunc("/v1/personas/obtenerId", h.obtenerID)
--- middleware/middleware.go
+++ middleware/middleware.go
@@ -8,6 +8,27 @@ import (
 func Log(funcion func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("peticion %q ,metodo %q", r.URL.Path, r.Method)
-		funcion(w, r)
+		funcion(w, r) // ejecutar la funcion
 	}
+}
+
+// funcion de autentificacion de el token
+func Autentificacion(funcion func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+		if token != "un-token-seguro" {
+			//devolver una respuesta
+			prohibido(w, r)
+			return
+
+		}
+		funcion(w, r) //ejecutar la funcion
+	}
+}
+
+func prohibido(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusForbidden)
+	w.Write([]byte("no tiene autorizacion"))
+
 }
