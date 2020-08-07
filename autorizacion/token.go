49f5154293a4c4a2ee21a461d324f6aeee507515
alvaro <pedro_alvaro@outlook.com.pe>
2020-08-06 23:05:40 -0500

    validacion de tokens

--- autorizacion/token.go
+++ autorizacion/token.go
@@ -1,6 +1,7 @@
 package autorizacion
 
 import (
+	"errors"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
@@ -22,4 +23,23 @@ func GenerarToken(data *modelo.Login) (string, error) {
 		return "", err
 	}
 	return tokenFirmado, nil
+}
+
+func ValidarToken(t string) (modelo.Claim, error) {
+	token, err := jwt.ParseWithClaims(t, &modelo.Claim{}, verificarFuncion)
+	if err != nil {
+		return modelo.Claim{}, err
+	}
+	if !token.Valid {
+		return modelo.Claim{}, errors.New("token no valido")
+	}
+	claim, ok := token.Claims.(*modelo.Claim)
+	if !ok {
+		return modelo.Claim{}, errors.New("no se puede obtener los claims")
+	}
+	return *claim, nil
+}
+
+func verificarFuncion(t *jwt.Token) (interface{}, error) {
+	return llaveVerificacion, nil
 }
