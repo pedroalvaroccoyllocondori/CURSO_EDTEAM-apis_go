40fe104cb1df8dec4082f7f6fb7a970e7d1a0bcb
alvaro <pedro_alvaro@outlook.com.pe>
2020-08-09 13:25:24 -0500

    grupo de rutas en echo

--- main.go
+++ main.go
@@ -13,12 +13,25 @@ func main() {
 	e.Use(middleware.Recover())
 	e.GET("/", saludar)
 	e.GET("/dividir", dividir)
+	// grupo de rutas
+	//e.POST("/personas/crear",crear)
+	//e.GET("/personas/consultar",consultar)
+	//e.PUT("/personas/actualizar",actualizar)
+	//e.DELETE("/personas/borrar",borrar)
+
+	personas := e.Group("/personas")
+	personas.POST("", crear)
+	personas.GET("/:id", consultar)
+	personas.PUT("/:id", actualizar)
+	personas.DELETE("/:id", borrar)
+
 	e.Start(":8080") //
 }
 
 func saludar(c echo.Context) error { // registrar una ruta por el metodo get
 	return c.String(http.StatusOK, "Hello, World!") // handler clasico
 }
+
 func dividir(c echo.Context) error {
 	d := c.QueryParam("d")
 	f, _ := strconv.Atoi(d)
@@ -28,4 +41,20 @@ func dividir(c echo.Context) error {
 	}
 	r := 3000 / f
 	return c.String(http.StatusOK, strconv.Itoa(r))
+}
+
+func crear(c echo.Context) error {
+	return c.String(http.StatusOK, "creado")
+}
+func consultar(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, "consultado"+id)
+}
+func actualizar(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, "actualizado"+id)
+}
+func borrar(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, "borrar"+id)
 }
